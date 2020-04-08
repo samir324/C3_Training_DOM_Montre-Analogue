8976e2c5e7f8514053944f5328970adf7be99f53
Samir Zahidi <samirreda324@gmail.com>
2020-04-08 15:48:37 +0100

    L'ajout du code javascript compatible

--- script.js
+++ script.js
@@ -3,24 +3,21 @@ const AIGUILLEHR = document.querySelector("#hour");
 const AIGUILLEMIN = document.querySelector("#minute");
 const AIGUILLESEC = document.querySelector("#second");
 
-//Extraire l'heure actuel à l'aide de l'objet Date()
 
-
-//Ajouter l'heure , minite , seconde  dans des varaiables
-
-
-
-// Calculer de degré de mouvement de l'aiguille heure, de l'aiguille minute, de l'aiguille seconde
-// Hint : Tous les aiguilles doivent se déplacer chaque second selon un degré
-
-
-// Déplacer les aiguilles 
 function demarrerLaMontre() {
+
+    //Extraire l'heure actuel à l'aide de l'objet Date()
     let date = new Date();
+
+    //Ajouter l'heure , minite , seconde  dans des varaiables
     let sec = date.getSeconds() / 60;
     let min = (date.getMinutes() + sec) / 60;
     let hour = (date.getHours() + min) / 12;
-    
+
+
+    // Calculer de degré de mouvement de l'aiguille heure, de l'aiguille minute, de l'aiguille seconde
+    // Hint : Tous les aiguilles doivent se déplacer chaque second selon un degré
+    // Déplacer les aiguilles 
     AIGUILLESEC.style.transform = "rotate(" + (sec * 360) + "deg)";
     AIGUILLEMIN.style.transform = "rotate(" + (min * 360) + "deg)";
     AIGUILLEHR.style.transform = "rotate(" + (hour * 360) + "deg)";
